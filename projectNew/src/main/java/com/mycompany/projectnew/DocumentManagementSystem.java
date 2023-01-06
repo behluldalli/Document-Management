a31c30a7f92f8c839900e9f75e8a42087a477f2f
behluldalli <behlul592003@hotmail.com>
2023-01-07 02:46:50 +0300

    Fixed 2

--- projectNew/src/main/java/com/mycompany/projectnew/DocumentManagementSystem.java
+++ projectNew/src/main/java/com/mycompany/projectnew/DocumentManagementSystem.java
@@ -14,52 +14,66 @@ import java.util.Scanner;
  */
 public class DocumentManagementSystem {
 
-  private final ArrayList<Document> documents;
-  private final Scanner scanner;
+    private final ArrayList<Document> documents;
+    private final Scanner scanner;
 
-  public DocumentManagementSystem() {
-    documents = new ArrayList<>();
-    scanner = new Scanner(System.in);
-  }
+    public DocumentManagementSystem() {
+        documents = new ArrayList<>();
+        scanner = new Scanner(System.in);
+        documents.add(new Document("PDF 1", new Date(15012023), "PDF"));
+        documents.add(new Document("PDF 2", new Date(16012023), "PDF"));
+        documents.add(new Document("PDF 3", new Date(17012023), "PDF"));
+        documents.add(new Document("Word 1", new Date(18012023), "Word"));
+        documents.add(new Document("Word 2", new Date(19012023), "Word"));
+        documents.add(new Document("Word 3", new Date(20012023), "Word"));
+        documents.add(new Document("Word 4", new Date(21012023), "Word"));
 
-  public void addDocument() {
-    System.out.print("Enter document name: ");
-    String name = scanner.nextLine();
-    System.out.print("Enter file type: ");
-    String fileType = scanner.nextLine();
-    documents.add(new Document(name, new Date(), fileType));
-    System.out.println("Document added successfully.");
-  }
-  
-   public void removeDocument() {
-    System.out.print("Enter document name: ");
-    String name = scanner.nextLine();
-    for (int i = 0; i < documents.size(); i++) {
-      if (documents.get(i).getName().equals(name)) {
-        documents.remove(i);
-        System.out.println("Document removed successfully.");
-        return;
-      }
+        System.out.println("Number of Documents " + documents.size());
+
+        for (int i = 0; i < documents.size(); i++) {
+            System.out.println(documents.get(i).getName());
+        }
+
     }
-    System.out.println("Document not found.");
-  }
-   
-   public void searchDocuments() {
-    System.out.print("Enter search query: ");
-    String query = scanner.nextLine();
-    ArrayList<Document> searchResults = new ArrayList<>();
-    for (Document doc : documents) {
-      if (doc.getName().contains(query)) {
-        searchResults.add(doc);
-      }
+
+    public void addDocument() {
+        System.out.print("Enter document name: ");
+        String name = scanner.nextLine();
+        System.out.print("Enter file type: ");
+        String fileType = scanner.nextLine();
+        documents.add(new Document(name, new Date(), fileType));
+        System.out.println("Document added successfully.");
     }
-    if (!searchResults.isEmpty()) {
-      System.out.println("Search results:");
-      for (Document doc : searchResults) {
-        System.out.println(doc.getName());
-      }
-    } else {
-      System.out.println("No results found.");
+
+    public void removeDocument() {
+        System.out.print("Enter document name: ");
+        String name = scanner.nextLine();
+        for (int i = 0; i < documents.size(); i++) {
+            if (documents.get(i).getName().equals(name)) {
+                documents.remove(i);
+                System.out.println("Document removed successfully.");
+                return;
+            }
+        }
+        System.out.println("Document not found.");
     }
-   }
+
+    public void searchDocuments() {
+        System.out.print("Enter search query: ");
+        String query = scanner.nextLine();
+        ArrayList<Document> searchResults = new ArrayList<>();
+        for (Document doc : documents) {
+            if (doc.getName().contains(query)) {
+                searchResults.add(doc);
+            }
+        }
+        if (!searchResults.isEmpty()) {
+            System.out.println("Search results:");
+            for (Document doc : searchResults) {
+                System.out.println(doc.getName());
+            }
+        } else {
+            System.out.println("No results found.");
+        }
+    }
 }
--- projectNew/src/main/java/com/mycompany/projectnew/Documents.java
+++ projectNew/src/main/java/com/mycompany/projectnew/Documents.java
@@ -15,38 +15,22 @@ import java.util.Scanner;
 class Documents {
 
     public static void main(String[] args) {
-    
-        ArrayList<Document> documents = new ArrayList<>();            
-       
-        documents.add(new Document("PDF 1", new Date(15012023), "PDF"));
-        documents.add(new Document("PDF 2", new Date(16012023), "PDF"));
-        documents.add(new Document("PDF 3", new Date(17012023), "PDF"));
-        documents.add(new Document("Word 1", new Date(18012023), "Word"));
-        documents.add(new Document("Word 2", new Date(19012023), "Word"));
-        documents.add(new Document("Word 3", new Date(20012023), "Word"));
-        documents.add(new Document("Word 4", new Date(21012023), "Word"));
-
-        System.out.println("Number of Documents " + documents.size());
-
-        for (int i = 0; i < documents.size(); i++) {
-            System.out.println(documents.get(i).getName());
-        }
-        
-        
-                DocumentManagementSystem doc = new DocumentManagementSystem();
+        DocumentManagementSystem doc = new DocumentManagementSystem();
         System.out.println("Please select 1 -> add, 2 -> delete, 3 -> search");
         Scanner sc = new Scanner(System.in);
         int b = sc.nextInt();
-        
+
         switch (b) {
-            case 1 -> doc.addDocument();
-            case 2 -> doc.removeDocument();
-            case 3 -> doc.searchDocuments();
+            case 1 ->
+                doc.addDocument();
+            case 2 ->
+                doc.removeDocument();
+            case 3 ->
+                doc.searchDocuments();
             default -> {
                 System.out.println("Hatali secim.");
-               
+
             }
         }
     }
-    }
-
+}
